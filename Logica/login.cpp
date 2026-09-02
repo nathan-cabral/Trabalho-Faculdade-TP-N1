0f2df0ea2791888baa713af8fa00ef0c0f81de7e
nathancabrall <nathaneliascabral@gmail.com>
2026-09-02 14:46:36 -0300

    criado sistema de login

---
+++ Logica/login.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    char nome[20];
+    int idade;
+    double peso;
+    cout<<"Nome: ";cin>>nome;
+    cout<<"idade: ";cin>>idade;
+    cout<<"peso: ";cin>>peso;
+
+    return 0;
+}

6baef14a422d75e8142b37b312a40a66dfd0b27b
nathancabrall <nathaneliascabral@gmail.com>
2026-09-02 14:38:05 -0300

    treino branch

--- Logica/new.cpp
+++ Logica/new.cpp
@@ -5,6 +5,12 @@ int main(){
     int x;
     cin>>x;
     cout<<x;
+    for(int i=0;i<x;i++){
+        cout<<i++<<"\n";
+    }
+    while(x<=x+10){
+        cout<<"X="<<x<<"\n";x++;
+    }
 
     return 0;
 }

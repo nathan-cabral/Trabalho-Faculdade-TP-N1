33b9328fca940afcab18f601a7a9ba2629398fe1
nathancabrall <nathaneliascabral@gmail.com>
2026-09-02 12:54:24 -0300

    nova alteracao

--- Logica/logica.c
+++ Logica/logica.c
@@ -1,6 +1,16 @@
 #include<stdio.h>
 void texto(){
-    printf("programa encerrado");
+    printf("programa encerrado\n");
+}
+
+int idade(int i){
+    if(i<13){
+        printf("crianca\n");
+    }else if(i<18){
+        printf("adolescente\n");
+    }else{
+        printf("adulto\n");
+    }
 }
 int main(){
     int vet[20];
@@ -8,6 +18,8 @@ int main(){
         scanf("%d",&vet[i]);
     }
     texto();
+    int idade;
+    scanf("%d",&idade);
 
     return 0;
 }
